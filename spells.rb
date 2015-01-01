210ba6b3383be908ef9ae46bb38be8f221fda678
Ken Walker <ken_walker@me.com>
2015-01-01 14:06:43 -0500

    few typos

--- spells.rb
+++ spells.rb
@@ -1,13 +1,9 @@
 require 'json'
-require 'pp'
 
 class Spells
   def initialize()
-  	puts "READ SPELLS"
 	json = File.read('spells.json')
 	@allSpells = JSON.parse(json)
-	percent = @allSpells.length / 161.0 * 100
-	puts "Finished #{@allSpells.length} Spells of 161 (#{percent} complete)"
   end
 
   def allSpells()
